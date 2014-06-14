e3592701bb6f8403e6e93c8a6442bb3a4231458a
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-06-14 10:11:31 -0500

    Working on arrays

--- encoding.go
+++ encoding.go
@@ -10,288 +10,310 @@ import (
 	"encoding/xml"
 )
 
+var nv = reflect.ValueOf(nil)
+
 type Field struct {
 	Name string
 	Value reflect.Value
 	Type reflect.Type
 }
 
-type MarshalFunction func(interface{}) ([]byte, error)
+func MakeField(name string, value interface{}, fType reflect.Type) Field {
+	return Field{name, reflect.ValueOf(value), fType}
+}
 
 type Marshaller interface {
 	MarshallableFields() []Field
 }
 
-func getValue(f Field) (interface{}, error) {
-	if f.Value.Kind() == reflect.Func {
-		t := f.Value.Type()
+func getValue(field Field) (interface{}, error) {
+	if field.Value.Kind() == reflect.Func {
+		fvType := field.Value.Type()
 		
-		if t.NumIn() != 0 {
-			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", f.Name,
-											  " should take no arguments but takes ", t.NumIn()))
+		if fvType.NumIn() != 0 {
+			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", field.Name,
+											 " should take no arguments but takes ", fvType.NumIn()))
 		}
 		
-		if t.NumOut() < 1 {
-			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", f.Name,
-											  " should return at least one value but returns ", t.NumOut()))
+		if fvType.NumOut() < 1 {
+			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", field.Name,
+											 " should return at least one value but returns ", fvType.NumOut()))
 		}
 		
-		if f.Type != nil && !t.Out(0).ConvertibleTo(f.Type) {
-			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", f.Name,
-											  "'s first return value should be of type ", f.Type,
-											  " but is of type ", t.Out(0)))
+		if field.Type != nil && !fvType.Out(0).ConvertibleTo(field.Type) {
+			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", field.Name,
+											 "'s first return value should be of type ", field.Type,
+											 " but is of type ", fvType.Out(0)))
 		}
 		
-		if t.NumOut() > 2 {
-			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", f.Name,
-											  " should return at most two values but returns ", t.NumOut()))
+		if fvType.NumOut() > 2 {
+			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", field.Name,
+											 " should return at most two values but returns ", fvType.NumOut()))
 		}
 		
-		if t.NumOut() == 2 && t.Out(1) != reflect.TypeOf(errors.New("")) {
-			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", f.Name,
-											  "'s second return value should be of type error but is of type ", t.Out(1)))
+		if fvType.NumOut() == 2 && fvType.Out(1) != reflect.TypeOf(errors.New("")) {
+			return nil, errors.New(fmt.Sprint("Function valued marshallable field ", field.Name,
+											 "'s second return value should be of type error but is of type ", fvType.Out(1)))
 		}
 		
-		r := f.Value.Call([]reflect.Value{})
+		rValue := field.Value.Call([]reflect.Value{})
 		
-		if len(r) > 1 && !r[1].IsNil() {
-			return nil, r[1].Interface().(error)
+		if len(rValue) > 1 && !rValue[1].IsNil() {
+			return nil, rValue[1].Interface().(error)
 		}
 		
-		f.Value = r[0]
+		field.Value = rValue[0]
 	}
 	
-	if f.Value.IsValid() {
-		if f.Type != nil {
-			f.Value = f.Value.Convert(f.Type)
-		}
-		return f.Value.Interface(), nil
+	if field.Value.IsValid() && field.Type != nil {
+		field.Value = field.Value.Convert(field.Type)
+	}
+	
+	if field.Value.IsValid() {
+		return field.Value.Interface(), nil
 	} else {
-		return reflect.Zero(f.Type), nil
+		return nil, nil
 	}
 }
 
-func Marshal(mf MarshalFunction, v interface{}) ([]byte, error) {
-	if _, ok := v.(Marshaller); !ok { return mf(v) }
+func Marshal(marshalFunc func(interface{}) ([]byte, error), value interface{}) ([]byte, error) {
+	rValue := reflect.ValueOf(value)
 	
-	m := make(map[string]string)
-	for _, f := range v.(Marshaller).MarshallableFields() {
-		i, err := getValue(f)
-		if err != nil { return nil, err }
+	if rValue.Kind() == reflect.Array {
+		count := rValue.Len()
+		array := make([]string, count)
 		
-		data, err := Marshal(mf, i)
-		if err != nil { return nil, err }
+		for i := 0; i < count; i++ {
+			data, err := Marshal(marshalFunc, rValue.Index(i).Interface())
+			if err != nil { return nil, err }
+			array[i] = string(data)
+		}
 		
-		m[f.Name] = string(data)
+		return marshalFunc(array)
 	}
 	
-	return mf(m)
+	if _, ok := value.(Marshaller); !ok { return marshalFunc(value) }
+	
+	strMap := make(map[string]string)
+	for _, field := range value.(Marshaller).MarshallableFields() {
+		obj, err := getValue(field)
+		if err != nil { return nil, err }
+		
+		data, err := Marshal(marshalFunc, obj)
+		if err != nil { return nil, err }
+		strMap[field.Name] = string(data)
+	}
+	
+	return marshalFunc(strMap)
 }
 
-func MarshalJSON(v interface{}) ([]byte, error) {
-	mf := json.Marshal
+func MarshalJSON(value interface{}) ([]byte, error) {
+	marshalFunc := json.Marshal
 	
-	if _, ok := v.(json.Marshaler); ok { return mf(v) }
-	if _, ok := v.(encoding.TextMarshaler); ok { return mf(v) }
+	if _, ok := value.(json.Marshaler); ok { return marshalFunc(value) }
+	if _, ok := value.(encoding.TextMarshaler); ok { return marshalFunc(value) }
 	
-	return Marshal(mf, v)
+	return Marshal(marshalFunc, value)
 }
 
-func MarshalXML(v interface{}) ([]byte, error) {
-	mf := xml.Marshal
+func MarshalXML(value interface{}) ([]byte, error) {
+	marshalFunc := xml.Marshal
 	
-	if _, ok := v.(xml.Marshaler); ok { return mf(v) }
-	if _, ok := v.(encoding.TextMarshaler); ok { return mf(v) }
+	if _, ok := value.(xml.Marshaler); ok { return marshalFunc(value) }
+	if _, ok := value.(encoding.TextMarshaler); ok { return marshalFunc(value) }
 	
-	return Marshal(mf, v)
+	return Marshal(marshalFunc, value)
 }
-
-type UnmarshalFunction func([]byte, interface{}) error
 
 type Unmarshaller interface {
 	UnmarshallableFields() []Field
 }
 
-func setField(f Field, v interface{}) error {
-	r := reflect.ValueOf(v)
+func setField(field Field, value interface{}) error {
+	rValue := reflect.ValueOf(value)
 	
-	if f.Type != nil {
-		if !r.Type().ConvertibleTo(f.Type) {
-			return errors.New(fmt.Sprint("Unmarshallable field is of type ", f.Type,
-										 " but value is of type ", r.Type()))
+	if field.Type != nil {
+		if !rValue.Type().ConvertibleTo(field.Type) {
+			return errors.New(fmt.Sprint("Unmarshallable field is of type ", field.Type,
+										 " but value is of type ", rValue.Type()))
 		}
-		r = r.Convert(f.Type)
+		rValue = rValue.Convert(field.Type)
 	}
 	
-	if f.Value.Kind() == reflect.Func {
-		t := f.Value.Type()
+	if field.Value.Kind() == reflect.Func {
+		fvType := field.Value.Type()
 		
-		if t.NumIn() != 1 {
-			return errors.New(fmt.Sprint("Function valued unmarshallable field ", f.Name,
-										 " should take one arguments but takes ", t.NumIn()))
+		if fvType.NumIn() != 1 {
+			return errors.New(fmt.Sprint("Function valued unmarshallable field ", field.Name,
+										 " should take one arguments but takes ", fvType.NumIn()))
 		}
 		
-		if !r.Type().ConvertibleTo(t.In(0)) {
-			return errors.New(fmt.Sprint("Function valued unmarshallable field ", f.Name,
-										 "'s first argument is of type ", t.In(0),
-										 " but value is of type ", r.Type()))
+		if !rValue.Type().ConvertibleTo(fvType.In(0)) {
+			return errors.New(fmt.Sprint("Function valued unmarshallable field ", field.Name,
+										 "'s first argument is of type ", fvType.In(0),
+										 " but value is of type ", rValue.Type()))
 		}
 		
-		if t.NumOut() > 1 {
-			return errors.New(fmt.Sprint("Function valued unmarshallable field ", f.Name,
-										 " should return at most one value but returns ", t.NumOut()))
+		if fvType.NumOut() > 1 {
+			return errors.New(fmt.Sprint("Function valued unmarshallable field ", field.Name,
+										 " should return at most one value but returns ", fvType.NumOut()))
 		}
 		
-		if t.NumOut() == 1 && t.Out(0).ConvertibleTo(reflect.TypeOf(errors.New(""))) {
-			return errors.New(fmt.Sprint("Function valued unmarshallable field ", f.Name,
-										 "'s first return value should be of type error but is of type ", t.Out(0)))
+		if fvType.NumOut() == 1 && fvType.Out(0).ConvertibleTo(reflect.TypeOf(errors.New(""))) {
+			return errors.New(fmt.Sprint("Function valued unmarshallable field ", field.Name,
+										 "'s first return value should be of type error but is of type ", fvType.Out(0)))
 		}
 		
-		//r = r.Convert(t.In(0))
-		e := f.Value.Call([]reflect.Value{r})
+		//r = rValue.Convert(t.In(0))
+		ret := field.Value.Call([]reflect.Value{rValue})
 		
-		if len(e) > 0 && !e[0].IsNil() {
-			return e[0].Interface().(error)
+		if len(ret) > 0 && !ret[0].IsNil() {
+			return ret[0].Interface().(error)
 		}
 		
 		return nil
 	}
 	
-	if f.Value.Kind() == reflect.Ptr {
-		f.Value = f.Value.Elem()
+	if field.Value.Kind() == reflect.Ptr {
+		field.Value = field.Value.Elem()
 		
-		if !f.Value.CanSet() {
-			return errors.New(fmt.Sprint("Pointer valued unmarshallable field ", f.Name,
+		if !field.Value.CanSet() {
+			return errors.New(fmt.Sprint("Pointer valued unmarshallable field ", field.Name,
 										 " should be settable but is not"))
 		}
 		
-		if !r.Type().ConvertibleTo(f.Value.Type()) {
-			return errors.New(fmt.Sprint("Pointer valued unmarshallable field ", f.Name,
-										 " is of type ", f.Value.Type(),
-										 " but value is of type ", r.Type()))
+		if !rValue.Type().ConvertibleTo(field.Value.Type()) {
+			return errors.New(fmt.Sprint("Pointer valued unmarshallable field ", field.Name,
+										 " is of type ", field.Value.Type(),
+										 " but value is of type ", rValue.Type()))
 		}
 		
-		f.Value.Set(r)
+		field.Value.Set(rValue)
 		
 		return nil
 	}
 	
-	return errors.New(fmt.Sprint("Unmarshallable field is of kind ", f.Value.Kind(),
+	return errors.New(fmt.Sprint("Unmarshallable field is of kind ", field.Value.Kind(),
 								 " but should be of kind Func or Ptr"))
 }
 
-func getSubclass(v interface{}, f Field) (interface{}, error) {
-	r := reflect.ValueOf(v)
+func getSubclass(field Field) (interface{}, error) {
+	if field.Value.Kind() == reflect.Func {
+		fvType := field.Value.Type()
+		
+		if fvType.NumIn() > 0 {
+			return nil, errors.New(fmt.Sprint("Function valued subclass field ", field.Name,
+											  " should take no arguments but takes ", fvType.NumIn()))
+		}
+		
+		if fvType.NumOut() < 1 {
+			return nil, errors.New(fmt.Sprint("Function valued subclass field ", field.Name,
+											  " should return at least one value but returns ", fvType.NumOut()))
+		}
+		
+		if field.Type != nil && !fvType.Out(0).ConvertibleTo(field.Type) {
+			return nil, errors.New(fmt.Sprint("Function valued subclass field ", field.Name,
+											  "'s first return value should be of type ", field.Type,
+											  " but is of type ", fvType.Out(0)))
+		}
+		
+		if fvType.NumOut() > 2 {
+			return nil, errors.New(fmt.Sprint("Function valued subclass field ", field.Name,
+											  " should return at most two values but returns ", fvType.NumOut()))
+		}
+		
+		if fvType.NumOut() == 2 && fvType.Out(1) != reflect.TypeOf(errors.New("")) {
+			return nil, errors.New(fmt.Sprint("Function valued subclass field ", field.Name,
+											  "'s second return value should be of type error but is of type ", fvType.Out(1)))
+		}
+		
+		ret := field.Value.Call([]reflect.Value{})
+		
+		if len(ret) > 1 && !ret[1].IsNil() {
+			return nil, ret[1].Interface().(error)
+		}
+		
+		field.Value = ret[0]
 	
-	if f.Type != nil {
-		if !r.Type().ConvertibleTo(f.Type) {
-			return nil, errors.New(fmt.Sprint("Subclass field is of type ", f.Type,
-											  " but value is of type ", r.Type()))
-		}
-		r = r.Convert(f.Type)
-	}
-	
-	if f.Value.Kind() == reflect.Func {
-		t := f.Value.Type()
-		
-		if t.NumIn() != 1 {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  " should take one arguments but takes ", t.NumIn()))
-		}
-		
-		if !r.Type().ConvertibleTo(t.In(0)) {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  "'s first argument is of type ", t.In(0),
-											  " but value is of type ", r.Type()))
-		}
-		
-		if t.NumOut() < 1 {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  " should return at least one value but returns ", t.NumOut()))
-		}
-		
-		if f.Type != nil && !t.Out(0).ConvertibleTo(f.Type) {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  "'s first return value should be of type ", f.Type,
-											  " but is of type ", t.Out(0)))
-		}
-		
-		if t.NumOut() > 2 {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  " should return at most two values but returns ", t.NumOut()))
-		}
-		
-		if t.NumOut() == 2 && t.Out(1) != reflect.TypeOf(errors.New("")) {
-			return nil, errors.New(fmt.Sprint("Function valued subclass field ", f.Name,
-											  "'s second return value should be of type error but is of type ", t.Out(1)))
-		}
-		
-		r := f.Value.Call([]reflect.Value{reflect.ValueOf(v)})
-		
-		if len(r) > 1 && !r[1].IsNil() {
-			return nil, r[1].Interface().(error)
-		}
-		
-		f.Value = r[0]
-	
-		if f.Value.IsValid() {
-			if f.Type != nil {
-				f.Value = f.Value.Convert(f.Type)
+		if field.Value.IsValid() {
+			if field.Type != nil {
+				field.Value = field.Value.Convert(field.Type)
 			}
-			return f.Value.Interface(), nil
+			return field.Value.Interface(), nil
 		} else {
-			return reflect.Zero(f.Type), nil
+			return reflect.Zero(field.Type), nil
 		}
 	}
 	
-	return nil, errors.New(fmt.Sprint("Subclass field is of kind ", f.Value.Kind(),
-								 " but should be of kind Func"))
+	return nil, errors.New(fmt.Sprint("Subclass field is of kind ", field.Value.Kind(),
+									  " but should be of kind Func"))
 }
 
-func Unmarshal(uf UnmarshalFunction, d []byte, v interface{}) error {
-	if _, ok := v.(Unmarshaller); !ok { return uf(d, v) }
+func Unmarshal(unmarshalFunc func([]byte, interface{}) error, data []byte, value interface{}) error {
+	rValue := reflect.ValueOf(value)
 	
-	m := make(map[string]string)
-	if err := uf(d, m); err != nil { return err }
-	
-loop:
-	for _, f := range v.(Unmarshaller).UnmarshallableFields() {
-		if f.Name == "" {
-			i, err := getSubclass(v, f)
+	if rValue.Kind() == reflect.Array {
+		eType := rValue.Type().Elem()
+		array := []string{}
+		
+		err := unmarshalFunc(data, array)
+		if err != nil { return err }
+		
+		for index, str := range array {
+			eValue := reflect.New(eType)
+			
+			err := Unmarshal(unmarshalFunc, []byte(str), eValue.Interface())
 			if err != nil { return err }
 			
-			if _, ok := i.(Unmarshaller); !ok {
+			rValue.Index(index).Set(eValue)
+		}
+		
+		return nil
+	}
+	
+	if _, ok := value.(Unmarshaller); !ok { return unmarshalFunc(data, value) }
+	
+	strMap := make(map[string]string)
+	if err := unmarshalFunc(data, strMap); err != nil { return err }
+	
+loop:
+	for _, field := range value.(Unmarshaller).UnmarshallableFields() {
+		if field.Name == "" {
+			subclass, err := getSubclass(field)
+			if err != nil { return err }
+			
+			if _, ok := subclass.(Unmarshaller); !ok {
 				return errors.New(fmt.Sprint("Subclass field does not implement gocoding.Unmarshaller"))
 			}
 			
-			v = i
+			value = subclass
 			
 			goto loop
 		}
 		
-		i := reflect.New(f.Type).Interface()
-		s := m[f.Name]
-		if err := Unmarshal(uf, []byte(s), i); err != nil { return err }
-		if err := setField(f, i); err != nil { return err }
+		obj := reflect.New(field.Type).Interface()
+		data := []byte(strMap[field.Name])
+		if err := Unmarshal(unmarshalFunc, data, obj); err != nil { return err }
+		if err := setField(field, obj); err != nil { return err }
 	}
 	
 	return nil
 }
 
-func UnmarshalJSON(d []byte, v interface{}) error {
-	uf := json.Unmarshal
+func UnmarshalJSON(data []byte, value interface{}) error {
+	unmarshalFunc := json.Unmarshal
 	
-	if _, ok := v.(json.Unmarshaler); ok { return uf(d, v) }
-	if _, ok := v.(encoding.TextUnmarshaler); ok { return uf(d, v) }
+	if _, ok := value.(json.Unmarshaler); ok { return unmarshalFunc(data, value) }
+	if _, ok := value.(encoding.TextUnmarshaler); ok { return unmarshalFunc(data, value) }
 	
-	return Unmarshal(uf, d, v)
+	return Unmarshal(unmarshalFunc, data, value)
 }
 
-func UnmarshalXML(d []byte, v interface{}) error {
-	uf := xml.Unmarshal
+func UnmarshalXML(data []byte, value interface{}) error {
+	unmarshalFunc := xml.Unmarshal
 	
-	if _, ok := v.(xml.Unmarshaler); ok { return uf(d, v) }
-	if _, ok := v.(encoding.TextUnmarshaler); ok { return uf(d, v) }
+	if _, ok := value.(xml.Unmarshaler); ok { return unmarshalFunc(data, value) }
+	if _, ok := value.(encoding.TextUnmarshaler); ok { return unmarshalFunc(data, value) }
 	
-	return Unmarshal(uf, d, v)
+	return Unmarshal(unmarshalFunc, data, value)
 }
